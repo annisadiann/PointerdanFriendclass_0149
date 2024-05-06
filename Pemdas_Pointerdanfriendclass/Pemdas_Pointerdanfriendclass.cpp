c9abb1153069ce20e0b73bee491debf43e29180b
annisadiann <annisadian0412@gmail.com>
2024-05-06 11:01:37 +0700

    Menambahkan contoh penggunaan referensi pointer untuk mengubah NIM objek Mahasiswa yang ada

--- Pemdas_Pointerdanfriendclass/Pemdas_Pointerdanfriendclass.cpp
+++ Pemdas_Pointerdanfriendclass/Pemdas_Pointerdanfriendclass.cpp
@@ -12,6 +12,10 @@ public:
 int main() {
     mahasiswa mhs{ 1 }; //Object mhs
     mhs.showNim(); //Member Access Operator
+
+    mahasiswa& refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNim();
 }
 
 
